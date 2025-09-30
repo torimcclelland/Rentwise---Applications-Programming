894fbdad2b54a531ac3346b4c39f612da20eda31
inesacosic <7inesacosic7@gmail.com>
2025-09-30 18:33:58 -0400

    initialized expo project

---
+++ app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, manual Expo setup 👋</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
