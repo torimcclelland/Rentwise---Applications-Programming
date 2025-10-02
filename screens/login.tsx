bf2df5ebe1fb0fbe908048360b5b6fe5f76019a6
inesacosic <7inesacosic7@gmail.com>
2025-10-01 22:17:53 -0400

    working on creating screen

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,9 +1,22 @@
 import React, {useState} from "react";
+import { Button, TextInput, View, Text, StyleSheet, Image } from "react-native";
 
 
 export default function login(){
     const [email, setEmail] = useState("");
     const [password, setPassword]= useState("");
 
-    
+    return (
+        <View>
+            <Image style={styles.logo} source={require('./rentwiseLogo.png')}/>
+            <Text>Rentwise</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create ({
+    logo: {
+        height: 84,
+        width: 74
+    }
+});
