cedbf7d66f7e8ebaa591faaf78327d710db82e6f
inesacosic <7inesacosic7@gmail.com>
2025-10-01 21:53:18 -0400

    created a basic login page

---
+++ screens/login.tsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+
+export default function login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword]= useState("");
+
+    
+}
