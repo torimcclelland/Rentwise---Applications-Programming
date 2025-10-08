0e22276b975428f33f801e87d7669d7c14bafcf9
torimcclelland <tori9991@hotmail.com>
2025-10-08 10:22:59 -0400

    creating components and adding imports/boilerplate

---
+++ components/BottomNavBar.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { TouchableOpacity, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import styles from '../styles/BottomNavBarStyle';
