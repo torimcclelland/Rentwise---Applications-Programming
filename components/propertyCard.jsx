eddbc0959982d52483a8d56ed7d3e67ae608e0f1
inesacosic <7inesacosic7@gmail.com>
2025-10-11 14:18:49 -0400

    completed the property card could use more styling

--- components/propertyCard.jsx
+++ components/propertyCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import {useState} from 'react'
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image, Text, Pressable } from 'react-native';
 
 
 const PropertyCard = ({address, ...props}) => {
     return (
         <View style={styles.box}>
-            <Image style={styles.image} source={require('../screens/rentwiseLogo.png')}/>
-            <Text style={styles.address}>{address}</Text>
+            <View style={styles.border}>
+                <Image style={styles.image} source={require('./house_2.webp')}/>
+                <View style={styles.button}>
+                    <Text style={styles.address}>{address}</Text>
+                    <Pressable>
+                        <Image style={styles.icon} source={require('./arrow_forward.svg')}/>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     );
 };
@@ -18,20 +25,34 @@ const styles = StyleSheet.create({
         height: 134,
         borderColor: 'gray',
         boxShadow: 'rgba(0, 0, 0, 0.25)',
-        textShadowOffset: { width: 0, height: 4 },
-        textShadowRadius: 4,
-        borderTopRightRadius: 12,
+        // textShadowOffset: { width: 0, height: 4 }, these are apparently depricated 
+        // textShadowRadius: 4,
+        borderWidth: 0.5,
+        height: 107,
+        width: 328,
+        borderWidth: 0.5,
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
         borderTopLeftRadius: 12,
-        flexShrink: 0
+        borderTopRightRadius: 12
+    },
+    button:{
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     image:{
         height: 80,
-        width: 328
+        width: 328,
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12
     },
-    text:{
-        backgroundColor: 'white'
+    address:{
+        backgroundColor: 'white',
+        paddingLeft: 8,
+        paddingTop: 2
+    },
+    icon:{
     }
-
 })
 
 export default PropertyCard;
