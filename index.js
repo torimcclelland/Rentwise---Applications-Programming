90d6630f951aa6f4e57b9718a1d8e0898473951f
torimcclelland <tori9991@hotmail.com>
2025-10-11 15:03:29 -0400

    restore index file to app login

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 import { registerRootComponent } from 'expo';
 import App from './app';
-<<<<<<< HEAD
-import LandlordPropertiesManager from './screens/LandlordPropertiesManager'
-
-registerRootComponent(LandlordPropertiesManager);
-=======
 import login from './screens/login'
 
 registerRootComponent(login);
->>>>>>> origin/main
