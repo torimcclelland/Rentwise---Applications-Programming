6ce81706b56b87ed5e9507781629e75ec3c971ea
inesacosic <7inesacosic7@gmail.com>
2025-10-18 21:36:07 -0400

    added navigation for property edit page

--- app.jsx
+++ app.jsx
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import login from './screens/login';
 import DashboardScreen from './screens/RenterDashboard';
 import LandlordPropertiesScreen from './screens/LandlordPropertiesManager';
+import PropertyEditScreen from './screens/propertyEditScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ function RootStack() {
       <Stack.Screen name="Login" component={login} />
       <Stack.Screen name="Renter Dashboard" component={DashboardScreen} />
       <Stack.Screen name="Landlord Dashboard" component={LandlordPropertiesScreen} />
+      <Stack.Screen name="Property Edit" component={PropertyEditScreen} />
     </Stack.Navigator>
   );
 }
