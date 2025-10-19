39a7b11b2f6c44531c75ccbedd9a0a11f8179274
Sasheanna <ssgroner@gmail.com>
2025-10-19 17:02:50 -0400

    Added navbar to various pages

--- screens/MessagesOverview.jsx
+++ screens/MessagesOverview.jsx
@@ -1,5 +1,11 @@
-
+import { View } from "react-native";
+import BottomNavBar from "../components/BottomNavBar";
 const MessagesOverview = () => {
-    return ;
+    return (
+        <View style={{ flex: 1 }}>
+        {/* Bottom Navigation Bar */}
+        <BottomNavBar selectedTab="messages" />
+        </View>
+    );
 }
 export default MessagesOverview;
--- screens/Notifications.jsx
+++ screens/Notifications.jsx
@@ -1,6 +1,12 @@
-
+import { View } from "react-native";
+import BottomNavBar from "../components/BottomNavBar";
 
 const Notifications = () => {
-    return ;
+    return (
+        <View style={{ flex: 1 }}>
+        {/* Bottom Navigation Bar */}
+        <BottomNavBar selectedTab="notifications" />
+        </View>
+    );
 }
 export default Notifications;
