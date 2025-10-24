7524847798fde01924e8c1256aee313ba580b4d0
torimcclelland <tori9991@hotmail.com>
2025-10-24 10:37:40 -0400

    loading page

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -2,10 +2,32 @@ import React from 'react';
 import { RotateLoader } from 'react-spinners';
 
 const Loader = () => (
-  <RotateLoader
-  color="#034974"
-  speedMultiplier={0.5}
-/>
+  <div style={{
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 9999
+  }}>
+    <RotateLoader
+      color="#034974"
+      speedMultiplier={0.5}
+    />
+    <p style={{
+      marginTop: '20px',
+      fontSize: '1.2rem',
+      color: '#034974',
+      fontFamily: 'sans-serif'
+    }}>
+      Hang tight! We're getting things ready for you!
+    </p>
+  </div>
 );
 
 export default Loader;
