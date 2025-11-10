e69c6e1d47363250590cbc8d21ca1d792f63e196
torimcclelland <tori9991@hotmail.com>
2025-11-10 11:00:19 -0500

    fixit model

---
+++ models/FixitRequest.js
@@ -0,0 +1,31 @@
+export class FixitRequest{
+
+    const maintenanceCategories = [
+        'Plumbing',
+        'Electrical',
+        'Heating/Cooling',
+        'Appliance Repair',
+        'Pest Control',
+        'General Repairs',
+        'Other',
+      ];
+
+    constructor({
+        userID = "",
+        explanation = "",
+        category = "",
+        submissontime = "",
+
+
+    } = {}){ 
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.isLandlord = isLandlord;
+        this.isPremUser = isPremUser;
+        this.properties = properties;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profilePicture = profilePicture
+    }
+}
